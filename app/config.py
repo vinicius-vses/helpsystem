63663b4f1dd2594e988ca197efe80e6de85e6440
mpsdecamargo <mpsdecamargo@gmail.com>
2024-11-08 10:24:25 -0300

    Adicionadas todas as rotas, alterados os templates, corrigido caminho das imagens, login funcional

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config:
-    #DATABASE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'db', 'help-system.db')
-    #DATABASE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db', 'help-system.db')
     DATABASE = os.path.join('..','db', 'help-system.db')
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
